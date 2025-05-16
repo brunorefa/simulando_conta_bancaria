3b06fd255d92152dc26921973b4bc22b8147c439
Bruno Refael <brusguario@gmail.com>
2025-05-15 22:14:53 -0300

    primeiro teste

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    public ContaTerminal() {
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+        System.out.println("Olá, seja bem-vindo ao Banco Digital!");
+        // Obter pela Scanner o número da conta digitada no termimal
+        System.out.println("Por favor, digite o número da conta:");
+        String numeroConta = scanner.next();
+        System.out.println("Por favor, digite o número da agência:");
+        String numeroAgencia = scanner.next();
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.next();
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldo = scanner.nextDouble();
+        // Obter pela Scanner os valores digitados no termimal
+        System.out.println("Olá, " + nomeCliente + "!", "seja bem-vindo ao Banco Digital!, sua conta foi criada com sucesso!");
+        // Exibir a mensagem conta criada com sucesso
+
+
     }
 }
